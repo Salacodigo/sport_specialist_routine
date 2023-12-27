69e97a6852c2785db182b3a33409892f7c0d374a
Santiago Salacodigo <santiago.salamanca7@gmail.com>
2023-12-27 00:04:14 -0500

    first version. video url to fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {
     rutinaFuerza
-} from './data.js'
+} from './javascript/data.js'
 
 
 const routineContainer = document.getElementById('routine-container');
@@ -21,60 +21,84 @@ function addEventListeners(){
 function showData(){
 
     cleanContainer(routineContainer);
+    if(rutinaFuerza.length<=0) return;
     
     for(let position in rutinaFuerza){
         console.log(rutinaFuerza[position]);
 
         const exerciseRowContainer = document.createElement('div');
+        exerciseRowContainer.classList.add('exercise-card');
 
         const dayBox = document.createElement('div');
+        dayBox.classList.add('dayBox');
         const exerciseBox = document.createElement('div');
+        exerciseBox.classList.add('exerciseBox');
         const seriesBox = document.createElement('div');
+        seriesBox.classList.add('seriesBox');
         const repsBox = document.createElement('div');
+        repsBox.classList.add('repsBox');
         const volumen_Box = document.createElement('div');
+        volumen_Box.classList.add('volumen_Box');
         const restBox = document.createElement('div');
+        restBox.classList.add('restBox');
         const videoBox = document.createElement('div');
+        videoBox.classList.add('videoBox');
         const video2Box = document.createElement('div');
+        video2Box.classList.add('video2Box');
 
         dayBox.innerHTML=`
-            ${rutinaFuerza[position].dia}
+            dia: ${rutinaFuerza[position].dia}
         `;
         exerciseBox.innerHTML=`
             ${rutinaFuerza[position].ejercicio}
         `;
         seriesBox.innerHTML=`
-            ${rutinaFuerza[position].series}
+            Series: ${rutinaFuerza[position].series}
         `;
         repsBox.innerHTML=`
-            ${rutinaFuerza[position].repeticiones}
+            Reps: ${rutinaFuerza[position].repeticiones}
         `;
         volumen_Box.innerHTML=`
-            ${rutinaFuerza[position].volumen_total}
+            Volumen: ${rutinaFuerza[position].volumen_total}
         `;
         restBox.innerHTML=`
-            ${rutinaFuerza[position].descanso_entre_series}
+            Descanso: ${rutinaFuerza[position].descanso_entre_series} s
         `;
+        let video_url = rutinaFuerza[position].video;
+        console.log({video_url});
         videoBox.innerHTML=`
-            ${rutinaFuerza[position].video}
+            <iframe width="440" height="230" src="https://www.youtube.com/embed/WJM6OKRxqq4?si=A0JWq1io3-9BboFe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            
         `;
         video2Box.innerHTML=`
-            ${rutinaFuerza[position].video2}
+            
         `;
         
+        const exerciseValuesSuperior = document.createElement('div');
+        exerciseValuesSuperior.classList.add('exerciseValuesSuperior');
+        
+        const exerciseValuesInferior = document.createElement('div');
+        exerciseValuesInferior.classList.add('exerciseValuesInferior');
 
-        exerciseRowContainer.appendChild(dayBox);
-        exerciseRowContainer.appendChild(exerciseBox);
-        exerciseRowContainer.appendChild(seriesBox);
-        exerciseRowContainer.appendChild(repsBox);
-        exerciseRowContainer.appendChild(volumen_Box);
-        exerciseRowContainer.appendChild(restBox);
+        // Video
         exerciseRowContainer.appendChild(videoBox);
         exerciseRowContainer.appendChild(video2Box);
-
+        exerciseRowContainer.appendChild(exerciseBox);
+        // Row values
+        exerciseValuesSuperior.appendChild(seriesBox);
+        exerciseValuesSuperior.appendChild(repsBox);
+        exerciseRowContainer.appendChild(exerciseValuesSuperior);
+        // 
+        
+        exerciseValuesInferior.appendChild(dayBox);
+        exerciseValuesInferior.appendChild(volumen_Box);
+        exerciseValuesInferior.appendChild(restBox);
+        exerciseRowContainer.appendChild(exerciseValuesInferior);
+        
+        //Add Card to container
         routineContainer.appendChild(exerciseRowContainer);
     }
 }
-
 
 
 function cleanContainer( container ){
